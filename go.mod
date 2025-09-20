2d7dcd945656ea21c4070814bbd2d8d04b47a050
giing <33764485+0xf0xx0@users.noreply.github.com>
2025-09-20 19:46:40 -0400

    refactor out dual-array garbage

--- go.mod
+++ go.mod
@@ -7,10 +7,17 @@ go 1.25.1
 require (
 	github.com/0xf0xx0/oigiki v1.0.1
 	github.com/Delta456/box-cli-maker/v2 v2.3.0
+	github.com/btcsuite/btcd v0.24.3-0.20250820175347-e8097a1b044c
 	github.com/gookit/color v1.5.4
 )
 
 require (
+	github.com/btcsuite/btcd/btcec/v2 v2.3.5 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
@@ -19,6 +26,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/term v0.0.0-20221017184919-83659145692c // indirect
+	golang.org/x/term v0.22.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"strings"
 
 	"github.com/0xf0xx0/oigiki"
 	"github.com/Delta456/box-cli-maker/v2"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/gookit/color"
 )
+
+const (
+	DATATYPE_UNK = iota
+	DATATYPE_OPCODE
+	DATATYPE_STRING
+	DATATYPE_INT
+	DATATYPE_FLOAT
+)
+
+type block struct {
+	Header, Body string
+	Type         int
+}
 
 func main() {
 	str := oigiki.ProcessTags(`
@@ -21,106 +36,130 @@ func main() {
 {/}└──────────────────────────┴──────┴────────────────────┘
 		`)
 	str = str
+	///strings.ToValidUTF8(s string, replacement string)
 	println(str)
-	// script, _ := hex.DecodeString("0297001a2f706f676f6c6f202d20666f73732069732066726565646f6d2f9359121200000000")
-	// tkzr := txscript.MakeScriptTokenizer(0, script)
-	// for {
-	// 	if tkzr.Done() {
-	// 		break
-	// 	}
-	// 	tkzr.Next()
-	// 	if err := tkzr.Err(); err != nil {
-	// 		println(err.Error())
-	// 		break
-	// 	}
-	// 	println(hex.EncodeToString([]byte{tkzr.Opcode()}),
-	// 		hex.EncodeToString(tkzr.Data()))
-	// }
-
-	// println(fmt.Sprintf("%s", strings.Join(mergeBoxes([]string{
-	// 	"OP_PUSH_2",
-	// 	"151",
-	// 	oigiki.ProcessTags("{cyan}OP_PUSH_26"),
-	// 	"/pogolo - foss is",
-	// }, []string{"02", oigiki.ProcessTags("{cyan}97 00"), "1a",
-	// 	"2f 70 6f 67 6f 6c 6f 20 2d 20 66 6f 73 73 20 69 73",}), "\n")))
-	// println(fmt.Sprintf("%s", strings.Join(mergeBoxes([]string{
-	// 	" freedom/",
-	// 	"OP_ADD",
-	// }, []string{"20 66 72 65 65 64 6f 6d 2f", "93"}), "\n")))
-
-	blocks := []string{
-		"OP_PUSH_2",
-		"151",
-		"OP_PUSH_26",
-		"/pogolo - foss is freedom/",
-		"OP_ADD",
-		"[error]",
+	script, _ := hex.DecodeString("0297001a2f706f676f6c6f202d20666f73732069732066726565646f6d2f9359121200000000")
+	tkzr := txscript.MakeScriptTokenizer(0, script)
+	reverseOpcodeMap := make(map[byte]string)
+	for k, v := range txscript.OpcodeByName {
+		reverseOpcodeMap[v] = k
 	}
-	b := make([][]string, 2)
-	h := make([][]string, 2)
-	headers := []string{
-		"02",
-		"97 00",
-		"1a",
-		"x2f 70 6f 67 6f 6c 6f 20 2d 20 66 6f 73 73 20 69 73 20 66 72 65 65 64 6f 6d 2f",
-		"93",
-		"59 12 12 00 00 00 00",
-	}
-	headerLen := 0
-	currLine := 0
-	for i, header := range headers {
-		x := len(header)
-		n := headerLen + x
-		if n > 80 && header[0] == 'x' {
-			x--
-			n--
-			header = header[1:]
-			/// break it
-			/// TODO: only if datatype is string
-			diff := n - 80
-			if x-diff < 0 {
-				break
-			}
-			blockOffset := diff * 3
-			headerOffset := x - blockOffset - diff - 3
-			// headerOffset := (x-diff)/2
-
-			blockChunk := blocks[i][blockOffset:]
-			headerChunk := strings.TrimSpace(header[headerOffset:])
-			blocks[i] = blocks[i][:blockOffset]
-			headers[i] = header[:headerOffset]
-
-			b[currLine] = blocks[:i+1]
-			h[currLine] = headers[:i+1]
-			currLine++
-			b[currLine] = append(b[currLine], blockChunk)
-			h[currLine] = append(h[currLine], headerChunk)
-			headerLen = 0
-		} else {
-			b[currLine] = append(b[currLine], blocks[i])
-			h[currLine] = append(h[currLine], headers[i])
+	blks := make([]block, 0, 20)
+	for {
+		if tkzr.Done() {
+			break
 		}
-		headerLen += x
+		tkzr.Next()
+		if err := tkzr.Err(); err != nil {
+			println(err.Error())
+			break
+		}
+		x := reverseOpcodeMap[tkzr.Opcode()]
+		blk := block{
+			Header: hex.EncodeToString([]byte{tkzr.Opcode()}),
+			Body:   x,
+			Type:   DATATYPE_OPCODE,
+		}
+		blks = append(blks, blk)
+		// println(fmt.Sprintf("%+v", blk))
+		if len(tkzr.Data()) > 0 {
+			hdr := prettyprintHex(tkzr.Data())
+			blk2 := block{
+				Header: hdr,
+			}
+			if strings.Contains(x, "DATA") {
+				blk2.Body = strings.ToValidUTF8(string(tkzr.Data()), "..")
+			}
+			// println(fmt.Sprintf("%+v", blk2))
+			blks = append(blks, blk2)
+		}
 	}
+	/// furst pushed bytes are the block height
+	blks[1].Type = DATATYPE_INT
+	/// next is the miner tag
+	blks[3].Type = DATATYPE_STRING
 
-	for x := range b {
-		// println(fmt.Sprintf("%v", b[x]))
-		println(fmt.Sprintf("%s", mergeBoxes(b[x], h[x])))
+	for _, line := range chunkData(blks) {
+		println(fmt.Sprintf("%s", mergeBoxes(line)))
 	}
 }
 
+func prettyprintHex(b []byte) string {
+	hex := hex.EncodeToString(b)
+	ret := ""
+	// println(len(hex))
+	for x := 0; x < len(hex); x += 2 {
+		ret += string([]byte{hex[x], hex[x+1]})
+		ret += " "
+	}
+	return strings.TrimSpace(ret)
+}
+func chunkData(blocks []block) [][]block {
+	ret := make([][]block, 1, 2)
+	headerLen := 0
+	currLine := 0
+	for _, header := range blocks {
+		x := len(header.Header)
+		n := headerLen + x
+		if n > 80 {
+			if header.Type == DATATYPE_STRING {
+				/// break it
+				for {
+					if n <= 80 {
+						break
+					}
+					diff := n - 80
+					if x-diff < 0 {
+						break
+					}
+					newBlk := block{Type: header.Type}
+					blockOffset := diff * 3
+					headerOffset := x - blockOffset - diff - 2
+					// headerOffset := (x-diff)/2
+					newBlk.Body = header.Body[:blockOffset]
+					newBlk.Header = header.Header[:headerOffset]
+					ret[currLine] = append(ret[currLine], newBlk)
+
+					bodyChunk := header.Body[blockOffset:]
+					headerChunk := strings.TrimSpace(header.Header[headerOffset:])
+
+					currLine++
+					ret = append(ret, make([]block, 0, 3))
+					newBlk = block{Type: header.Type}
+					newBlk.Body = bodyChunk
+					newBlk.Header = headerChunk
+					ret[currLine] = append(ret[currLine], newBlk)
+
+					headerLen = 0
+					x = len(headerChunk)
+					n = diff
+				}
+			} else {
+				currLine++
+				ret = append(ret, make([]block, 0, 3))
+				ret[currLine] = append(ret[currLine], header)
+				headerLen = 0
+			}
+		} else {
+			ret[currLine] = append(ret[currLine], header)
+		}
+		headerLen += x
+	}
+	return ret
+}
+
 func padString(str string, l int) string {
-	x := len(color.ClearCode(str))
+	x := len(str)
 	if x < l {
 		spaces := (l - x) / 2
 		ret := strings.Repeat(" ", spaces) + str + strings.Repeat(" ", l-(spaces+x))
 		return ret
 	}
+	// println("str: ", str)
 	return str
 }
 
-func mergeBoxes(boxes []string, headlines []string) string {
+func mergeBoxes(boxes []block) string {
 	b := box.New(
 		box.Config{
 			ContentAlign: "Center",
@@ -129,23 +168,24 @@ func mergeBoxes(boxes []string, headlines []string) string {
 	)
 	ret := make([]string, 4)
 	if len(boxes) == 1 {
-		str := padString(boxes[0], len(color.ClearCode(headlines[0])))
+		/// TODO: figure out how to add ansi here???
+		str := padString(boxes[0].Body, len(color.ClearCode(boxes[0].Header)))
 		split := strings.Split(b.String("", str), "\n")
-		if headlines[0] == "" {
+		if boxes[0].Header == "" {
 			ret[0] += strings.Repeat(" ", len(split[1])-6)
 		} else {
 			/// ansi MUST be cleared here for proper len
-			ret[0] += " " + padString(headlines[0], len(color.ClearCode(split[1]))-6)
+			ret[0] += " " + padString(boxes[0].Header, len(color.ClearCode(split[1]))-6)
 		}
 		ret[1] = split[0]
 		ret[2] = split[1]
 		ret[3] = split[2]
 		return strings.Join(ret, "\n")
 	}
-	for i, str := range boxes {
+	for i, blk := range boxes {
 		/// ansi MUST be cleared here for proper len
-		str = padString(str, len(color.ClearCode(headlines[i])))
-		str = b.String("", str)
+		str := padString(blk.Body, len(color.ClearCode(boxes[i].Header)))
+		str = b.String("", colorText(blk.Type, str))
 		split := strings.Split(str, "\n")
 		/// box height is always 3
 		if i == 0 {
@@ -170,14 +210,38 @@ func mergeBoxes(boxes []string, headlines []string) string {
 			temp = temp[:len(temp)-3]
 			ret[3] += temp
 		}
-		if headlines[i] == "" {
+		if boxes[i].Header == "" {
 			ret[0] += strings.Repeat(" ", len(split[1])-6)
 		} else {
 			/// ansi MUST be cleared here for proper len
-			ret[0] += " " + padString(headlines[i], len(color.ClearCode(split[1]))-6)
+			ret[0] += " " + padString(boxes[i].Header, len(color.ClearCode(split[1]))-6)
 		}
 	}
 	return strings.Join(ret, "\n")
+}
+func colorText(datatype int, text string) string {
+	color := ""
+	switch datatype {
+	case DATATYPE_INT:
+		fallthrough
+	case DATATYPE_FLOAT:
+		{
+			color = "blue"
+		}
+	case DATATYPE_STRING:
+		{
+			color = "green"
+		}
+	case DATATYPE_OPCODE:
+		{
+			color = "yellow"
+		}
+	case DATATYPE_UNK:
+		{
+			color = "red"
+		}
+	}
+	return oigiki.ProcessTags(fmt.Sprintf("{%s}%s{/}", color, text))
 }
 
 /// TODO: test file
